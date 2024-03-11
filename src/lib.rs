acddef9b01d714bed60083333dd570cb8c1ab0df
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-03-11 20:13:27 +0100

    Added iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::{error::Error, fs};
+use std::error::Error;
+use std::fs;
 
 #[derive(Debug)]
 pub struct Config {
@@ -7,13 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Query not found"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("File path not found"),
+        };
 
         Ok(Config { query, file_path })
     }
@@ -36,11 +42,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut result = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line)
-        }
-    }
+    result.extend(contents.lines().filter(|line| line.contains(query)));
 
     result
 }
